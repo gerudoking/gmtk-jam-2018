06c090301353b94b1fd0f456f44490829c7a4de9
Paulo M C Lopes <zeitron@users.noreply.github.com>
2018-09-01 17:38:40 -0300

    commit pra parar de bugar

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -9,6 +9,12 @@ public class Node {
 		
 	}
 	
+    //G cost = distance from start node
+    //H cost (heurist) = distance from target
+    //F cost = G + H
+    //choose the one with the lowest F cost
+    //if both nodes have the same cost, choose the one if lowest H cost
+
 	// Update is called once per frame
 	void Update () {
 		
